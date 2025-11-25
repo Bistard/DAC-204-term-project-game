cf763bfa3a15778f7aa6453bccf57408b1f2e7b3
SIHAN LI <lshh1015813038@gmail.com>
2025-11-24 23:12:24 -0500

    chore

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GameProvider } from './context/GameContext';
-import { GameLayout } from './components/gameLayout1';
+import { GameLayout } from './components/GameLayout';
 import { DebugConsole } from './components/DebugConsole';
 
 const App: React.FC = () => {
